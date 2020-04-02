a01a1a7b04ba5d8224440c28909cbb4174a09868
Philipe <philipe991@gmail.com>
2020-04-02 14:43:00 -0300

    meu primeiro commit

---
+++ js/main.js
@@ -0,0 +1,103 @@
+/* Aula 01
+var nome = "Philipe Melo";
+var idade = 29;
+var idade2 = 10;
+var frase = "Japão é o melhor time do mundo."
+// alert("Nome: " + nome + " Idade: " + idade + " anos.");
+//alert(idade+idade2);
+
+// Imprimir variáveis no console do navegador
+console.log(nome);
+console.log(idade+idade2);
+
+// Substituir pedeço de string no console
+// console.log(frase.replace("Japão","Brasil"));
+
+// Substituir pedeço de string no alert
+// alert(frase.replace("Japão","Brasil"));
+
+Tornar todas as palavras da string maiúsculas
+alert(frase.toLocaleUpperCase());
+console.log(frase.toLocaleUpperCase());
+
+
+// Tornar todas as palavras da string minúsculas
+alert(frase.toLocaleLowerCase());
+console.log(frase.toLocaleLowerCase());
+*/
+
+/* Aula 02 - Array*/
+
+var lista = ["maçã", "pêra", "laranja"];
+/* imprimir a lsita inteira
+console.log(lista);
+*/
+
+/* imprimir apenas um elemento da lista
+console.log(lista[1])
+alert(lista[1]);
+*/
+
+/*
+// incluir novo item na lista
+lista.push("uva");
+lista.push("banana")
+// remover o último item da lista
+lista.pop();
+*/
+
+/* Ver o tamanho da lista
+console.log(lista.length);
+*/
+
+/* Imprimir a lista ao contrário
+console.log(lista.reverse());
+*/
+
+/* tornar array em string
+console.log(lista.toString());
+*/
+
+/* imprimir a lista e em seguida transformar ela em string, listando o primeiro elemento da string
+console.log(lista);
+console.log(lista.toString()[0]);
+*/
+
+/* alterar o separador da string de , para -
+console.log(lista);
+console.log(lista.toString());
+console.log(lista.join("-"));
+*/
+
+// aula 02 - Dicionário
+
+/*
+var fruta = {nome: "maçã", cor: "vermelho"}
+//imprime todo o dicionário
+console.log(fruta);
+//imprime somente o atributo cor
+alert(fruta.cor);
+*/
+
+// criar um array de dicionário
+var frutas = [{nome: "maçã", cor: "vermelho"},{nome:"uva", cor:"roxa"}]
+//imprime todo o dicionário
+console.log(frutas);
+//imprime somente o atributo cor
+alert(frutas[1].cor);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
